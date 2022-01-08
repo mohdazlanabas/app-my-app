fa33991484c13f2c3807622fc8d21c302900ee6d
Azlan Abas <95009591+mohdazlanabas@users.noreply.github.com>
2022-01-08 11:48:59 +0800

    Add files via upload

---
+++ src/Components/PersonList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+ 
+export default class PersonList extends React.Component{
+    sate ={
+        persons: [],
+    };
+
+componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+        console.log(res);
+        this.setState({persons: res.data});
+    });
+}
+
+render() {
+    return <ul> {this.state.persons.map (person => <li> {person.name} </li>)} </ul>;
+}}
+
